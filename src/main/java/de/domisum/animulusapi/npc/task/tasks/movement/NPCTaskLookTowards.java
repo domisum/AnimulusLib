ef59a12f200d6dbc61f25764b1c5eed176ef5894
Dominik <dominik.weissenseel@gmx.de>
2016-09-06 15:11:32 +0200

    Fixed look towards task

--- src/main/java/de/domisum/animulusapi/npc/task/tasks/movement/NPCTaskLookTowards.java
+++ src/main/java/de/domisum/animulusapi/npc/task/tasks/movement/NPCTaskLookTowards.java
@@ -3,7 +3,6 @@ package de.domisum.animulusapi.npc.task.tasks.movement;
 import de.domisum.animulusapi.npc.task.NPCTask;
 import de.domisum.animulusapi.npc.task.NPCTaskSlot;
 import de.domisum.auxiliumapi.util.java.annotations.APIUsage;
-import de.domisum.auxiliumapi.util.math.MathUtil;
 import org.bukkit.Location;
 
 @APIUsage
@@ -20,9 +19,6 @@ public class NPCTaskLookTowards extends NPCTask
 	private float targetYaw;
 	private float targetPitch;
 	private double speedMultiplier;
-
-	// STATUS
-	private int yawRotationDirection = 0;
 
 
 	// -------
@@ -61,17 +57,7 @@ public class NPCTaskLookTowards extends NPCTask
 	@Override
 	protected void onStart()
 	{
-		float dYaw = (this.targetYaw-this.npc.getLocation().getYaw()+360f)%360f;
 
-		if(dYaw < 180)
-			this.yawRotationDirection = 1;
-		else
-			this.yawRotationDirection = -1;
-
-		/*if(dYaw < 0)
-			throw new IllegalStateException(
-					"dYaw has illegal value: "+dYaw+" locationYaw: "+this.npc.getLocation().getYaw()+" targetYaw: "
-							+this.targetYaw);*/
 	}
 
 	@Override
@@ -79,20 +65,29 @@ public class NPCTaskLookTowards extends NPCTask
 	{
 		Location currentLocation = this.npc.getLocation();
 
-		float dYaw = ((this.targetYaw-currentLocation.getYaw())%360f+360f)%360f;
+		float dYaw = (this.targetYaw-currentLocation.getYaw())%360;
+		if(dYaw < 0)
+			dYaw += 360;
 		if(dYaw > 180)
 			dYaw -= 360;
+
 		float dPitch = this.targetPitch-currentLocation.getPitch();
 
 		if(Math.abs(dYaw)+Math.abs(dPitch) < TOLERANCE)
 			return false;
 
-		float stepDYaw = (dPitch < 0 ? -1 : 1)*(float) MathUtil.minAbs(BASE_SPEED*this.speedMultiplier, dYaw);
-		float stepDPitch = (dPitch < 0 ? -1 : 1)*(float) Math.min(BASE_SPEED*this.speedMultiplier, Math.abs(dPitch));
+		float stepDYaw = getClampedDelta(dYaw);
+		float stepDPitch = getClampedDelta(dPitch);
 
 		this.npc.setYawPitch(currentLocation.getYaw()+stepDYaw, currentLocation.getPitch()+stepDPitch);
 		return true;
 	}
+
+	private float getClampedDelta(float value)
+	{
+		return (float) ((value < 0 ? -1 : 1)*Math.min(Math.abs(value), BASE_SPEED*this.speedMultiplier));
+	}
+
 
 	@Override
 	protected void onCancel()
